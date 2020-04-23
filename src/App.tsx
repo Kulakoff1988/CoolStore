21537338dcfca4ff808f63f4d16f36a5b9910721
Alexander Kulakov <kulakofft4@gmail.com>
2020-04-23 22:44:09 +0300

    project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import { Table } from "./components/GoodsTable";
+import { Select } from "./components/BrandSelect";
+import { Pagination } from "./components/Pagination";
+import { TGood } from "./interfaces/types";
+import { goods as data } from "./serviceData/goods";
 
-function App() {
+const App: React.FunctionComponent = () => {
+  const [goods, setGoods] = useState<TGood[]>([]);
+  const [options, setOptions] = useState<string[]>([]);
+  const [activeBrand, setActiveBrand] = useState<string>(`-1`);
+  const [startIndex, setStartIndex] = useState<number>(0);
+
+  useEffect(() => {
+    setGoods(data);
+    const options = Array.from(new Set(data.map((good) => good.brand)));
+    setOptions(options);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='caption'>Cool Store</div>
+      <div style={{ display: `flex` }}>
+        <Table
+          goods={goods
+            .filter((good) => activeBrand === `-1` || good.brand === activeBrand)
+            .slice(startIndex, startIndex + 4)}
+        />
+        <div className='filter-handler'>
+          <Select options={options} active={activeBrand} setActive={(value) => setActiveBrand(value)} />
+          <button onClick={() => setActiveBrand(`-1`)}>Сбросить фильтры</button>
+        </div>
+      </div>
+      <Pagination
+        startIndex={startIndex}
+        setStartIndex={(index: number) => setStartIndex(index)}
+        count={goods.filter((good) => activeBrand === `-1` || good.brand === activeBrand).length}
+      />
     </div>
   );
-}
+};
 
 export default App;
